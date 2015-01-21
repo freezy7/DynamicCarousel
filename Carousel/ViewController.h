ed220b84054d102e000c8da9ecc2398f2b8c2286
freezy7 <453901962@qq.com>
2015-01-21 10:31:59 +0800

    Initial Commit

---
+++ Carousel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Carousel
+//
+//  Created by R_style Man on 15-1-21.
+//  Copyright (c) 2015年 R_style Man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Carousel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Carousel
+//
+//  Created by R_style Man on 15-1-21.
+//  Copyright (c) 2015年 R_style Man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
